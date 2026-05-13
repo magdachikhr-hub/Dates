3483caf3a7fb09e3e6c72526c5b51aae46751dca
magda chikhradz <magda292005@gmail.com>
2026-05-13 14:53:53 +0400

    setTimeOut

---
+++ script.js
@@ -0,0 +1,9 @@
+const contact = document.querySelector(".contact");
+
+contact.addEventListener("click", () => {
+  window.location.href = "./contacts.html";
+});
+
+// setTimeout() - function , function name
+
+setTimeout(() => {}, 1000);
