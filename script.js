490ebf44314cb1083ff1ff264b5d6bd94c58d109
magda chikhradz <magda292005@gmail.com>
2026-05-13 17:25:48 +0400

    setInterval

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const contact = document.querySelector(".contact");
 
-contact.addEventListener("click", () => {
-  window.location.href = "./contacts.html";
-});
+// contact.addEventListener("click", () => {
+//   window.location.href = "./contacts.html";
+// });
 
 // setTimeout() - function , function name
 
 setTimeout(() => {}, 1000);
+
+setTimeout(() => {
+  console.log("magda");
+}, 10000);
+
+setInterval(() => {
+  console.log("hi");
+}, 3000);
+
+// || - logikuri an
+
+const notify = document.querySelector(".notify");
+const register = document.querySelector("#register");
+
+register.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // console.log(register);
+  const nameValue = document.getElementById("name").value;
+
+  if (nameValue.length < 4 || nameValue.length > 15) {
+    return console.log("error");
+  }
+
+  notify.classList.add("show");
+});
