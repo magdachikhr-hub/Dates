8f15af362913db652ff6d679b6f1a795667e5772
magda chikhradz <magda292005@gmail.com>
2026-05-15 16:28:23 +0400

    random advice generator

--- DOM_4/main.js
+++ DOM_4/main.js
@@ -1,13 +1,18 @@
+const idHtml = document.querySelector(".advice_id");
+const adviceHtml = document.querySelector(".advice_html");
+const button = document.querySelector(".advice_button");
+
 async function getQuote() {
   const response = await fetch("https://api.adviceslip.com/advice");
   console.log(response);
   const data = await response.json();
   console.log(data.slip);
-
-
-  
+  adviceHtml.textContent = `"${data.slip.advice}"`;
+  idHtml.textContent = `ADVICE # ${data.slip.id}`;
 }
 
-getQuote();
+button.addEventListener("click", () => {
+  getQuote();
+});
 
 // async and await - always used together
